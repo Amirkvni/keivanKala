58190cb7807ffc72529b4e3aff595cf159418e74
amirhosein keivani <amirhosein8292@gmail.com>
2025-07-12 19:22:53 +0330

    refactor: optimize MongoDB connection with caching for better performance in serverless environment

--- configs/db.js
+++ configs/db.js
@@ -1,13 +1,30 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
+
+let cached = global.mongoose;
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 const connectToDB = async () => {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGO_URL, {
+      dbName: "keivankala",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  }
+
   try {
-    if (mongoose.connections[0].readyState) {
-      return true;
-    } else {
-      await mongoose.connect(process.env.MONGO_URL);
-    }
-  } catch (error) {
-    console.log("DB connection has error =>", error);
+    cached.conn = await cached.promise;
+    console.log("db vasl shod");
+
+    return cached.conn;
+  } catch (err) {
+    console.error("❌ DB connection error =>", err);
+    throw err;
   }
 };
+
 export default connectToDB;
