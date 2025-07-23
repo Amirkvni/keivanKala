0f991008f9eb69df9b26b49a572a83ae4da4d887
amirhosein keivani <amirhosein8292@gmail.com>
2025-07-23 16:48:27 +0330

    style(admin): design product creation page UI layout

--- src/app/(admin)/dashboard/product-creation/page.jsx
+++ src/app/(admin)/dashboard/product-creation/page.jsx
@@ -1,7 +1,132 @@
+import ProductImageUploader from "@/components/templates/dashboard/ProductImageUploader";
 import React from "react";
+import { FaTrashAlt } from "react-icons/fa";
 
 function page() {
-  return <div className="p-12">page</div>;
+  return (
+    <div className="p-12">
+      <span className="text-xl font-bold">ایجاد محصول</span>
+      <div className="flex gap-x-2 mt-5 [&>div]:p-4 [&>div]:rounded-lg ">
+        <div className="w-8/12  flex flex-col gap-y-4  [&>div]:p-3">
+          <div className="bg-white flex flex-col gap-y-6 [&>div]:flex [&>div]:flex-col [&>div]:gap-y-2 [&>div>input]:bg-gray-200 [&>div>input]:p-2 [&>div>input]:rounded-sm [&>div>input]:outline-none ">
+            <span>اطلاعات پایه</span>
+            <div className="mt-4">
+              <label htmlFor="">نام کالا (فارسی)</label>
+              <input type="text" />
+            </div>
+            <div className="">
+              <label htmlFor="">نام کالا (انگلیسی)</label>
+              <input type="text" />
+            </div>
+            <div className="">
+              <label htmlFor="">کد کالا</label>
+              <input type="text" />
+            </div>
+          </div>
+          <div className="bg-white">
+            <div className="flex justify-between items-center">
+              <span>ویژگی های محصول</span>
+              <button className="bg-green-400 rounded-lg p-2 cursor-pointer text-white">
+                افزودن
+              </button>
+            </div>
+            <div className="mt-3 [&>div>input]:outline-none [&>div>input]:bg-gray-200 [&>div>input]:p-2 [&>div>input]:rounded-sm flex flex-col gap-y-3  [&>div]:flex [&>div]:gap-x-3 [&>div]:items-center">
+              <div>
+                <input type="text" placeholder="عنوان" />
+                <span>:</span>
+                <input type="text" placeholder="توضیحات" />
+              </div>
+              <div>
+                <input type="text" placeholder="عنوان" />
+                <span>:</span>
+                <input type="text" placeholder="توضیحات" />
+              </div>
+            </div>
+          </div>
+          <div className="grid grid-cols-2 gap-x-5 [&>div]:bg-white [&>div]:p-4 [&>div]:rounded-lg ">
+            <div>
+              <div className="flex justify-between items-center">
+                <span>رنگ ها</span>
+                <button className="bg-green-400 rounded-lg p-2 cursor-pointer text-white">
+                  افزودن
+                </button>
+              </div>
+              <div className="mt-3 [&>div]:flex [&>div]:gap-x-3 [&>div]:items-center flex flex-col gap-y-3 ">
+                <div>
+                  <label htmlFor="">رنگ اول</label>
+                  <span>:</span>
+                  <select name="" id="">
+                    <option value="">قرمز</option>
+                    <option value="">زرد</option>
+                    <option value="">نارنجی</option>
+                  </select>
+                </div>
+                <div>
+                  <label htmlFor="">رنگ دوم</label>
+                  <span>:</span>
+                  <select name="" id="">
+                    <option value="">قرمز</option>
+                    <option value="">زرد</option>
+                    <option value="">نارنجی</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div className="[&>div]:flex  ">
+              <span>قیمت گذاری</span>
+              <div className="mt-4 flex-col gap-y-6 [&>div]:flex [&>div]:gap-x-3 [&>div]:items-center [&>div>input]:outline-none">
+                <div>
+                  <label htmlFor="">قیمت :</label>
+                  <input type="text" />
+                </div>
+                <div>
+                  <label htmlFor=""> قیمت تخفیف :</label>
+                  <input type="text" />
+                </div>
+                <div>
+                  <label htmlFor="">درصد تخفیف(%) :</label>
+                  <input type="text" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-4/12  flex flex-col gap-y-4  [&>div]:p-4  [&>div]:rounded-lg ">
+          <div className="flex flex-col gap-y-6 [&>span]:text-xs [&>span]:text-gray-500 bg-white">
+            <p>تصویر محصول</p>
+            <span>
+              یک عکس محصول انتخاب کنید یا به سادگی تا ۵ عکس را اینجا بکشید و رها
+              کنید.
+            </span>
+            <ProductImageUploader />
+            <span>
+              فرمت‌های تصویر: .jpg, .jpeg, .png، اندازه ترجیحی: ۱:۱، حداکثر حجم
+              فایل: ۵۰۰ کیلوبایت.
+            </span>
+          </div>
+          <div className="bg-white">
+            <p>ویژگی ها</p>
+            <div className=" mt-4 flex flex-col gap-y-4 [&>div]:flex [&>div]:flex-col [&>div]:gap-y-2 [&>div>input]:bg-gray-200 [&>div>input]:p-2 [&>div>input]:rounded-sm [&>div>input]:outline-none ">
+              <div>
+                <label htmlFor="">دسته‌بندی</label>
+                <input type="text" />
+              </div>
+              <div>
+                <label htmlFor="">برند</label>
+                <input type="text" />
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-end gap-x-3 [&>button]:p-2 [&>button]:rounded-lg [&>button]:cursor-pointer  ">
+            <button className="flex items-center gap-x-2 border-red-500 border text-red-500">
+              دور انداحتن <FaTrashAlt className="text-red-500" />
+            </button>
+            <button className="bg-green-500  text-white">ایجاد کنید</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default page;
---
+++ src/components/templates/dashboard/ProductImageUploader.jsx
@@ -0,0 +1,84 @@
+"use client";
+import { useDropzone } from "react-dropzone";
+import { useEffect, useState } from "react";
+import { FaImage, FaTrashAlt } from "react-icons/fa";
+
+export default function ProductImageUploader({ onDrop }) {
+  const [files, setFiles] = useState([]);
+
+  const handleDelete = (index) => {
+    const updatedFiles = [...files];
+    URL.revokeObjectURL(updatedFiles[index].preview);
+    updatedFiles.splice(index, 1);
+    setFiles(updatedFiles);
+    if (onDrop) onDrop(updatedFiles);
+  };
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop: (acceptedFiles) => {
+      const mappedFiles = acceptedFiles.map((file) =>
+        Object.assign(file, {
+          preview: URL.createObjectURL(file),
+        })
+      );
+      const allFiles = [...files, ...mappedFiles];
+      setFiles(allFiles);
+      if (onDrop) onDrop(allFiles);
+    },
+  });
+
+  useEffect(() => {
+    return () => {
+      files.forEach((file) => URL.revokeObjectURL(file.preview));
+    };
+  }, [files]);
+
+  return (
+    <section className="w-full">
+      {/* Dropzone */}
+      <div
+        {...getRootProps({
+          className:
+            "flex flex-col items-center justify-center border-2 border-dashed border-blue-400 rounded-md p-6 cursor-pointer text-center text-gray-600 hover:bg-blue-50 transition",
+        })}
+      >
+        <input {...getInputProps()} />
+        <FaImage className="text-4xl text-blue-400 mb-3" />
+        <p className="text-sm">تصاویر خود را اینجا رها کنید یا</p>
+        <p className="text-sm text-blue-500 underline">برای مرور کلیک کنید</p>
+      </div>
+
+      {files.length > 0 && (
+        <div className="mt-4 grid grid-cols-3  gap-4 cursor-pointer">
+          {files.map((file, index) => (
+            <div
+              key={index}
+              className="border border-dashed border-blue-400 rounded-lg p-2 shadow-sm relative group"
+            >
+              <button
+                onClick={() => handleDelete(index)}
+                className="absolute top-2 right-2 bg-white p-1 rounded-full shadow-md text-red-500 hover:bg-red-100 transition z-10 opacity-0 group-hover:opacity-100"
+                title="حذف تصویر"
+              >
+                <FaTrashAlt className="w-4 h-4 cursor-pointer" />
+              </button>
+
+              <img
+                src={file.preview}
+                alt={file.name}
+                className="w-full h-32 object-cover rounded-md mb-2"
+              />
+
+              <div className="text-xs text-center text-gray-700">
+                <p className="font-medium truncate">{file.name}</p>
+                <p className="text-gray-500">
+                  {(file.size / 1024).toFixed(1)} KB
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </section>
+  );
+}
