fbf920165424840db07fa5e633ab68cad44af4f4
amirhosein keivani <amirhosein8292@gmail.com>
2025-08-16 20:37:12 +0330

    feat(dashboard): optimize DashboardStatsCards component logic

--- src/components/templates/dashboard/DashboardStatsCards.jsx
+++ src/components/templates/dashboard/DashboardStatsCards.jsx
@@ -3,45 +3,141 @@ import { FaMoneyBillTrendUp } from "react-icons/fa6";
 import { PiShoppingBagBold } from "react-icons/pi";
 import { FiUsers } from "react-icons/fi";
 import { LuTicket } from "react-icons/lu";
-import connectToDB from "@/configs/db";
 import UserModel from "@/models/User";
 import TicketModel from "@/models/Ticket";
 import OrderModel from "@/models/Order";
+import PaymentModel from "@/models/Payment";
 export default async function DashboardStatsCards() {
-  connectToDB();
   const usersCount = await UserModel.countDocuments();
   const ticketsCount = await TicketModel.countDocuments({
     mainTicket: { $exists: false },
   });
   const startOfToday = new Date();
   startOfToday.setHours(0, 0, 0, 0);
-
   const endOfToday = new Date();
   endOfToday.setHours(23, 59, 59, 999);
 
+  const yesterday = new Date();
+  yesterday.setDate(yesterday.getDate() - 1);
+  const startOfYesterday = new Date(
+    yesterday.getFullYear(),
+    yesterday.getMonth(),
+    yesterday.getDate(),
+    0,
+    0,
+    0,
+    0
+  );
+  const endOfYesterday = new Date(
+    yesterday.getFullYear(),
+    yesterday.getMonth(),
+    yesterday.getDate(),
+    23,
+    59,
+    59,
+    999
+  );
   const todayOrderCount = await OrderModel.countDocuments({
     createdAt: {
       $gte: startOfToday,
       $lt: endOfToday,
     },
   });
+  const yesterdayOrderCount = await OrderModel.countDocuments({
+    createdAt: {
+      $gte: startOfYesterday,
+      $lt: endOfYesterday,
+    },
+  });
+  const todayUserCount = await UserModel.countDocuments({
+    createdAt: {
+      $gte: startOfToday,
+      $lt: endOfToday,
+    },
+  });
+  const yesterdayUserCount = await UserModel.countDocuments({
+    createdAt: {
+      $gte: startOfYesterday,
+      $lt: endOfYesterday,
+    },
+  });
+  const todayAgg = await PaymentModel.aggregate([
+    {
+      $match: {
+        orderDate: { $gte: startOfToday, $lte: endOfToday },
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        totalPaid: { $sum: { $toDouble: "$paid" } },
+      },
+    },
+  ]);
+  const yesterdayAgg = await PaymentModel.aggregate([
+    { $match: { orderDate: { $gte: startOfYesterday, $lte: endOfYesterday } } },
+    { $group: { _id: null, totalPaid: { $sum: { $toDouble: "$paid" } } } },
+  ]);
+  const todayTicketCount = await TicketModel.countDocuments({
+    mainTicket: { $exists: false },
+    createdAt: {
+      $gte: startOfToday,
+      $lt: endOfToday,
+    },
+  });
+  const yesterdayTicketCount = await TicketModel.countDocuments({
+    mainTicket: { $exists: false },
+    createdAt: {
+      $gte: startOfYesterday,
+      $lt: endOfYesterday,
+    },
+  });
+  const todayIncome = todayAgg[0]?.totalPaid || 0;
+  const yesterdayIncome = yesterdayAgg[0]?.totalPaid || 0;
+  let incomeGrowthPercent = 0;
+  let orderGrowthCount = todayOrderCount - yesterdayOrderCount;
+  let userGrowthCount = todayUserCount - yesterdayUserCount;
+  let ticketGrowthCount = todayTicketCount - yesterdayTicketCount;
+  if (yesterdayIncome > 0) {
+    incomeGrowthPercent =
+      ((todayIncome - yesterdayIncome) / yesterdayIncome) * 100;
+  } else if (yesterdayIncome === 0) {
+    incomeGrowthPercent = todayIncome > 0 ? 100 : 0;
+  } else {
+    incomeGrowthPercent =
+      ((todayIncome - yesterdayIncome) / Math.abs(yesterdayIncome)) * 100;
+  }
 
   return (
     <div className="grid grid-cols-4 gap-x-4 [&>div]:p-3 [&>div]:rounded-sm [&>div]:bg-white">
       <DashboardStatCard
         title="درآمد امروز"
-        value="۶۰۷۶۵۹۶ تومان"
-        percentage="+16.24%"
-        percentColor="text-green-500"
+        value={`${todayIncome.toLocaleString() || 0} تومان `}
+        percentage={`${incomeGrowthPercent}%${
+          incomeGrowthPercent >= 0 ? "+" : "-"
+        } `}
+        percentColor={`${
+          incomeGrowthPercent > 0
+            ? "text-green-500"
+            : incomeGrowthPercent === 0
+            ? "text-gray-500"
+            : "text-red-500"
+        }`}
         description="مشاهده کل درآمد"
         icon={<FaMoneyBillTrendUp />}
         iconBg="bg-blue-500 text-blue-200"
       />
       <DashboardStatCard
         title="سفارشات امروز"
-        value={`${todayOrderCount} تا `}
-        percentage="-32%"
-        percentColor="text-red-500"
+        value={`${todayOrderCount.toLocaleString() || 0} تا `}
+        percentage={`${orderGrowthCount}${orderGrowthCount >= 0 ? "+" : "-"} `}
+        percentColor={`${
+          orderGrowthCount > 0
+            ? "text-green-500"
+            : orderGrowthCount === 0
+            ? "text-gray-500"
+            : "text-red-500"
+        }`}
         address="/dashboard/all-orders"
         description="مشاهده همه سفارش‌ها"
         icon={<PiShoppingBagBold />}
@@ -50,8 +146,14 @@ export default async function DashboardStatsCards() {
       <DashboardStatCard
         title="کل کاربران"
         value={`${usersCount} نفر `}
-        percentage="+2"
-        percentColor="text-green-500"
+        percentage={userGrowthCount}
+        percentColor={`${
+          userGrowthCount > 0
+            ? "text-green-500"
+            : userGrowthCount === 0
+            ? "text-gray-500"
+            : "text-red-500"
+        }`}
         address="/dashboard/all-users"
         description="مشاهده همه کاربران"
         icon={<FiUsers />}
@@ -60,8 +162,14 @@ export default async function DashboardStatsCards() {
       <DashboardStatCard
         title=" کل تیکت‌ها"
         value={`${ticketsCount} تا `}
-        percentage="+1"
-        percentColor="text-green-500"
+        percentage={ticketGrowthCount}
+        percentColor={`${
+          ticketGrowthCount > 0
+            ? "text-green-500"
+            : ticketGrowthCount === 0
+            ? "text-gray-500"
+            : "text-red-500"
+        }`}
         description="مشاهده همه تیکت‌ها"
         address="/dashboard/all-tickets"
         icon={<LuTicket />}
